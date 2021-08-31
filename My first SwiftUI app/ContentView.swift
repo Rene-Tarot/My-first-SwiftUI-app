8b5600fa506bd302c098a9b9db8c235bf2570dfc
Rene-Tarot <rene.tarot@galileo.edu>
2021-08-30 22:17:04 -0600

    Initial Commit

---
+++ My first SwiftUI app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My first SwiftUI app
+//
+//  Created by Rene Tarot on 8/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My first SwiftUI app/My_first_SwiftUI_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_first_SwiftUI_appApp.swift
+//  My first SwiftUI app
+//
+//  Created by Rene Tarot on 8/30/21.
+//
+
+import SwiftUI
+
+@main
+struct My_first_SwiftUI_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
